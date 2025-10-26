2fb76cf0bbf900ed0db58f91aa5172d48d9a16c9
Brian Rivera <alexisrb2408@gmail.com>
2025-10-25 21:46:13 -0500

    Paso 12, 13 creando y modficando archivos

--- javascript1.js
+++ javascript1.js
@@ -3,3 +3,19 @@ console.log('Script 1 cargado');
 function inicializar() {
     console.log('Aplicación inicializada');
 }
+function mostrarBienvenida() {
+    console.log('Bienvenido al Proyecto Final 2');
+    console.log('Grupo: JVBTS');
+}
+
+// Función para obtener información del proyecto
+function obtenerInfoProyecto() {
+    return {
+        nombre: 'Avance Proyecto Final 2',
+        grupo: 'JVBTS',
+        version: '1.0'
+    };
+}
+
+// Ejecutar al cargar
+mostrarBienvenida();
